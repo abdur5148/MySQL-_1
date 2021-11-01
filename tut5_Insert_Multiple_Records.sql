a995bdef8d734b009efddbb762bed8b1bb703fe1
Abdur Rehman Ansari <abdur5148@icloud.com>
2021-11-01 20:28:50 +0530

    Add files via upload

---
+++ tut5_Insert_Multiple_Records.sql
@@ -0,0 +1,8 @@
+insert into personal(id,name,age,gender,phone,city)
+values
+(1,"Ram Kumar",19,"M",4022155,"Agra"),
+(2,"Sarita Kumari",21,"F",4034421,"Delhi"),
+(3,"Salman Khan",20,"M",4056221,"Agra"),
+(4,"Juhi Chawla",18,"F",4089821,"Bhopal"),
+(5,"Anil Kapoor",22,"M",4025221,"Agra"),
+(6,"John Abraham",21,"M",4033776,"Delhi");
